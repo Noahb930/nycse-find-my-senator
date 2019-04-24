733b57db861634d57aca315c45edac0db6b1916a
Noah Getz <noahb930@gmail.com>
2019-04-24 15:34:29 -0400

    debugging

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,15 @@ end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
+Bundler.require(:default, :assets, Rails.env)
 module NYCSEResearch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
